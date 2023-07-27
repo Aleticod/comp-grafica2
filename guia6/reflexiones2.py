736139ec8440f83c46f4dbb893f23a1728e8a081
Aleticod <193837@unsaac.edu.pe>
2023-07-26 23:32:50 -0500

    Modify guia6: Uncomment file save line

--- guia6/reflexiones2.py
+++ guia6/reflexiones2.py
@@ -1,239 +1,131 @@
-"""
-A simple raytracer that supports spheres with configurable color properties
-(like the base color and specular coefficient).
-"""
+# Importamos librerias
+import numpy as np
+import matplotlib.pyplot as plt
 
-import math
+# Funcion para normalizar un vector
+def normalizar(vector):
+    return vector / np.linalg.norm(vector)
 
-class Scene:
-	"""
-	The scene that gets rendered. Contains information like the camera
-	position, the different objects present, etc.
-	"""
+# Funcion que devuelve vector reflejado
+def reflejado(vector, eje):
+    return vector - 2 * np.dot(vector, eje) * eje
 
-	def __init__(self, camera, objects, lights, width, height):
-		self.camera = camera
-		self.objects = objects
-		self.lights = lights
-		self.width = width
-		self.height = height
+# Funcion de interseccion con la esfera
+def interseccion_esfera(centro, radio, origen_rayo, direccion_rayo):
+    b = 2 * np.dot(direccion_rayo, origen_rayo - centro)
+    c = np.linalg.norm(origen_rayo - centro) ** 2 - radio ** 2
+    # Calculo de discriminante
+    delta = b ** 2 - 4 * c
+    if delta > 0:
+        # Calculo de los puntos de inteseccion
+        t1 = (-b + np.sqrt(delta)) / 2
+        t2 = (-b - np.sqrt(delta)) / 2
+        if t1 > 0 and t2 > 0:
+            # Retornamos el que se encuentra mas cerca
+            return min(t1, t2)
+    return None
 
-	def render(self):
-		"""
-		Return a `self.height`x`self.width` 2D array of `Color`s representing
-		the color of each pixel, obtained via ray-tracing.
-		"""
+# Funcion del objeto mas cercano intersectado
+def objeto_intersectado_mas_cercano(objetos, origen_rayo, direccion_rayo):
+    distancias = [interseccion_esfera(obj['centro'], obj['radio'], origen_rayo, direccion_rayo) for obj in objetos]
+    objeto_mas_cercano = None
+    distancia_minima = np.inf
+    for indice, distancia in enumerate(distancias):
+        if distancia and distancia < distancia_minima:
+            distancia_minima = distancia
+            objeto_mas_cercano = objetos[indice]
+    return objeto_mas_cercano, distancia_minima
 
-		pixels = [
-			[Color() for _ in range(self.width)] for _ in range(self.height)]
+# Definios la dimensiones de la pantalla
+ancho = 300
+altura = 200
 
-		for y in range(self.height):
-			for x in range(self.width):
-				ray_direction = Point(x, y) - self.camera
-				ray = Ray(self.camera, ray_direction)
-				pixels[y][x] = self._trace_ray(ray)
+profundidad_maxima = 3
 
-		return pixels
+# Ubicacion de la camara
+camara = np.array([0, 0.7, 0.9])
+ratio = float(ancho) / altura
+pantalla = (-1, 1 / ratio, 1, -1 / ratio) # izquierda, arriba, derecha, abajo
 
-	def _trace_ray(self, ray, depth=0, max_depth=5):
-		"""
-		Recursively trace a ray through the scene, returning the color it
-		accumulates.
-		"""
+# Ubicacion la fuente de luz
+luz = { 'posicion': np.array([-15, 10, -0.5]), 'ambiente': np.array([1, 1, 1]), 'difuso': np.array([1, 1, 1]), 'especular': np.array([1, 1, 1]) }
 
-		color = Color()
+# Definicion de los objetos
+objetos = [
+{ 'centro': np.array([-0.3, -0.5, -1.3]), 'radio': 0.2, 'ambiente': np.array([0.1, 0, 0]), 'difuso': np.array([1.0, 0, 0]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([0, -0.5, -0.8]), 'radio': 0.2, 'ambiente': np.array([0.1, 0, 0.1]),'difuso': np.array([0, 0.6, 0]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([-0.3, -0.5, -0.5]), 'radio': 0.2, 'ambiente': np.array([0, 0.1, 0]), 'difuso': np.array([1, 1, 0]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([0.1, -0.5, -0.3]), 'radio': 0.2, 'ambiente': np.array([0, 0, 0.1]), 'difuso': np.array([0.07, 0.15, 0.5]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([-0.75, -0.5, -0.55]), 'radio': 0.2, 'ambiente': np.array([0, 0, 0.1]), 'difuso': np.array([1, 0.5, 0]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([-0.5, -0.5, -1]), 'radio': 0.2, 'ambiente': np.array([0, 0, 0.1]), 'difuso': np.array([0, 0, 0.6]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([0.1, -0.5, -1.4]), 'radio': 0.2, 'ambiente': np.array([0, 0, 0.1]), 'difuso': np.array([0, 0, 0.6]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([-0.74, -0.5, -1.4]), 'radio': 0.2, 'ambiente': np.array([0, 0, 0.1]), 'difuso': np.array([0.84, 0.32, 0.07]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([-0.4, -0.5, -1.6]), 'radio': 0.2, 'ambiente': np.array([0, 0.1, 0]), 'difuso': np.array([0, 0.4, 0]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([0, -0.5, -1.95]), 'radio': 0.2, 'ambiente': np.array([0, 0, 0.1]), 'difuso': np.array([1.0, 0.2, 0]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([-0.45, -0.5, -2.59]), 'radio': 0.2, 'ambiente': np.array([0, 0, 0.1]), 'difuso': np.array([0, 0, 0.9]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([-0.95, -0.5, -2.4]), 'radio': 0.2, 'ambiente': np.array([0, 0, 0.1]),'difuso': np.array([0.4, 1.0, 0]), 'especular': np.array([1, 1, 1]), 'brillo': 100, 'reflexion': 0 },
+{ 'centro': np.array([0, -9000, 0]), 'radio': 9000 - 0.7, 'ambiente': np.array([0.1, 0.1, 0.1]), 'difuso': np.array([0.6, 0.6, 0.6]), 'especular': np.array([1, 1, 1]),'brillo': 100, 'reflexion': 0.5 }
+]
 
-		if depth >= max_depth:
-			return color
+# Inicializamos una matriz para que almacene los pixeles de la imagen
+imagen = np.zeros((altura, ancho, 3))
+for i, y in enumerate(np.linspace(pantalla[1], pantalla[3], altura)):
+    for j, x in enumerate(np.linspace(pantalla[0], pantalla[2], ancho)):
+        # pantalla esta en el origen
+        pixel = np.array([x, y, 0])
+        origen = camara
+        direccion = normalizar(pixel - origen)
+        # Definimos los colores para cada pixel
+        color = np.zeros((3))
+        reflexion = 1
 
-		intersection = self._get_intersection(ray)
-		if intersection is None:
-			return color
+        for k in range(profundidad_maxima):
+            # verificar pro intersecciones
+            objeto_mas_cercano, distancia_minima = objeto_intersectado_mas_cercano(objetos, origen, direccion)
+            if objeto_mas_cercano is None:
+                break
 
-		obj, dist = intersection
-		intersection_pt = ray.point_at_dist(dist)
-		surface_norm = obj.surface_norm(intersection_pt)
+            # Calculo de las intersecciones
+            interseccion = origen + distancia_minima * direccion
+            normal_a_superficie = normalizar(interseccion - objeto_mas_cercano['centro'])
+            punto_desplazado = interseccion + 1e-5 * normal_a_superficie   
+            interseccion_con_luz = normalizar(luz['posicion'] - punto_desplazado)
 
-		# ambient light
-		color += obj.material.color * obj.material.ambient
+            # Calculo de la distancia minima
+            _, distancia_minima = objeto_intersectado_mas_cercano(objetos, punto_desplazado, interseccion_con_luz)
+            interseccion_con_luz_distancia = np.linalg.norm(luz['posicion'] - interseccion)
+            esta_sombreado = distancia_minima < interseccion_con_luz_distancia
 
-		# lambert shading
-		for light in self.lights:
-			pt_to_light_vec = (light - intersection_pt).normalize()
-			pt_to_light_ray = Ray(intersection_pt, pt_to_light_vec)
-			if self._get_intersection(pt_to_light_ray) is None:
-				lambert_intensity = surface_norm * pt_to_light_vec
-				if lambert_intensity > 0:
-					color += obj.material.color * obj.material.lambert * \
-						lambert_intensity
+            # Verificacion si hay una sombra
+            if esta_sombreado:
+                break
 
-		# specular (reflective) light
-		reflected_ray = Ray(
-			intersection_pt, ray.direction.reflect(surface_norm).normalize())
-		color += self._trace_ray(reflected_ray, depth + 1) * \
-			obj.material.specular
-		return color
+            # Definimoms la iluminacion
+            iluminacion = np.zeros((3))
 
-	def _get_intersection(self, ray):
-		"""
-		If ray intersects any of `self.objects`, return `obj, dist` (the object
-		itself, and the distance to it). Otherwise, return `None`.
-		"""
+            # Calculo de la luz ambiental
+            iluminacion += objeto_mas_cercano['ambiente'] * luz['ambiente']
 
-		intersection = None
-		for obj in self.objects:
-			dist = obj.intersects(ray)
-			if dist is not None and \
-				(intersection is None or dist < intersection[1]):
-				intersection = obj, dist
+            # Calculo de la luz difusa
+            iluminacion += objeto_mas_cercano['difuso'] * luz['difuso'] * np.dot(interseccion_con_luz, normal_a_superficie)
 
-		return intersection
+            # Calculo de la luz especular
+            interseccion_a_camara = normalizar(camara - interseccion)
+            H = normalizar(interseccion_con_luz + interseccion_a_camara)
+            iluminacion += objeto_mas_cercano['especular'] * luz['especular'] * np.dot(normal_a_superficie, H) ** (objeto_mas_cercano['brillo'] / 4)
 
-class Vector:
-	"""
-	A generic 3-element vector. All of the methods should be self-explanatory.
-	"""
+            # Calculo de la reflexion
+            color += reflexion * iluminacion
+            reflexion *= objeto_mas_cercano['reflexion']
 
-	def __init__(self, x=0, y=0, z=0):
-		self.x = x
-		self.y = y
-		self.z = z
+            origen = punto_desplazado
+            direccion = reflejado(direccion, normal_a_superficie)
 
-	def norm(self):
-		return math.sqrt(sum(num * num for num in self))
-
-	def normalize(self):
-		return self / self.norm()
-
-	def reflect(self, other):
-		other = other.normalize()
-		return self - 2 * (self * other) * other
-
-	def __str__(self):
-		return "Vector({}, {}, {})".format(*self)
-
-	def __add__(self, other):
-		return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
-
-	def __sub__(self, other):
-		return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
-
-	def __mul__(self, other):
-		if isinstance(other, Vector):
-			return self.x * other.x + self.y * other.y + self.z * other.z;
-		else:
-			return Vector(self.x * other, self.y * other, self.z * other)
-
-	def __rmul__(self, other):
-		return self.__mul__(other)
-
-	def __truediv__(self, other):
-		return Vector(self.x / other, self.y / other, self.z / other)
-
-	def __pow__(self, exp):
-		if exp != 2:
-			raise ValueError("Exponent can only be two")
-		else:
-			return self * self
-
-	def __iter__(self):
-		yield self.x
-		yield self.y
-		yield self.z
-
-# Since 3D points and RGB colors are effectively 3-element vectors, we simply
-# declare them as aliases to the `Vector` class to take advantage of all its
-# defined operations (like overloaded addition, multiplication, etc.) while
-# improving readability (so we can use `color = Color(0xFF)` instead of
-# `color = Vector(0xFF)`).
-Point = Vector
-Color = Vector
-
-class Sphere:
-	"""
-	A sphere object.
-	"""
-
-	def __init__(self, origin, radius, material):
-		self.origin = origin
-		self.radius = radius
-		self.material = material
-
-	def intersects(self, ray):
-		"""
-		If `ray` intersects sphere, return the distance at which it does;
-		otherwise, `None`.
-		"""
-
-		sphere_to_ray = ray.origin - self.origin
-		b = 2 * ray.direction * sphere_to_ray
-		c = sphere_to_ray ** 2 - self.radius ** 2
-		discriminant = b ** 2 - 4 * c
-
-		if discriminant >= 0:
-			dist = (-b - math.sqrt(discriminant)) / 2
-			if dist > 0:
-				return dist
-
-	def surface_norm(self, pt):
-		"""
-		Return the surface normal to the sphere at `pt`.
-		"""
-
-		return (pt - self.origin).normalize()
-
-class Material:
-
-	def __init__(self, color, specular=0.5, lambert=1, ambient=0.2):
-		self.color = color
-		self.specular = specular
-		self.lambert = lambert
-		self.ambient = ambient
-
-class Ray:
-	"""
-	A mathematical ray.
-	"""
-
-	def __init__(self, origin, direction):
-		self.origin = origin
-		self.direction = direction.normalize()
-
-	def point_at_dist(self, dist):
-		return self.origin + self.direction * dist
-
-def pixels_to_ppm(pixels):
-	"""
-	Convert `pixels`, a 2D array of `Color`s, into a PPM P3 string.
-	"""
-
-	header = "P3 {} {} 255\n".format(len(pixels[0]), len(pixels))
-	img_data_rows = []
-	for row in pixels:
-		pixel_strs = [
-			" ".join([str(int(color)) for color in pixel]) for pixel in row]
-		img_data_rows.append(" ".join(pixel_strs))
-	return header + "\n".join(img_data_rows)
-
-if __name__ == "__main__":
-	objects = [
-		Sphere(
-			Point(150, 120, -20), 80, Material(Color(0xFF, 0, 0),
-			specular=0.2)),
-		Sphere(
-			Point(420, 120, 0), 100, Material(Color(0, 0, 0xFF),
-			specular=0.8)),
-		Sphere(Point(320, 240, -40), 50, Material(Color(0, 0xFF, 0))),
-		Sphere(
-			Point(300, 200, 200), 100, Material(Color(0xFF, 0xFF, 0),
-			specular=0.8)),
-		Sphere(Point(300, 130, 100), 40, Material(Color(0xFF, 0, 0xFF))),
-		Sphere(Point(300, 1000, 0), 700, Material(Color(0xFF, 0xFF, 0xFF),
-			lambert=0.5)),
-		]
-	lights = [Point(200, -100, 0), Point(600, 200, -200)]
-	camera = Point(200, 200, -400)
-	scene = Scene(camera, objects, lights, 600, 400)
-	pixels = scene.render()
-	with open("image.ppm", "w") as img_file:
-		img_file.write(pixels_to_ppm(pixels))
+        # Generamos la imagen
+        imagen[i, j] = np.clip(color, 0, 1)
+    
+    print("%d/%d" % (i + 1, altura))
+# Guargar e imprimir la imagen
+plt.imsave('imagen2.png', imagen)
+implot = plt.imshow(imagen)
+plt.show()
